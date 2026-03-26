27c4ddaed205408e2dd3850c7720b85a632ca236
Darwin Alvarez <112340199+Darwinalvarez123@users.noreply.github.com>
2026-03-26 00:30:22 +0000

    Docs: nuevos servicios

--- src/main/java/edu/unimagdalena/lms/domine/service/ServiceInstructorImp.java
+++ src/main/java/edu/unimagdalena/lms/domine/service/ServiceInstructorImp.java
@@ -1,7 +1,12 @@
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import jakarta.transaction.Transactional;
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class ServiceInstructorImp implements ServiceInstructor {
 
 }
--- src/main/java/edu/unimagdalena/lms/domine/service/mapper/InstructorMapper.java
+++ src/main/java/edu/unimagdalena/lms/domine/service/mapper/InstructorMapper.java
@@ -1,21 +1,21 @@
 import java.time.Instant;
 
+import edu.unimagdalena.lms.api.dto.InstructorDto;
 import edu.unimagdalena.lms.domine.entities.Instructor;
 
 public class InstructorMapper {
-    public static Instructor toEntity(InstructorCreateReques req) {
-        return Instructor.builder().email(req.mail()).fullName(req.name()).created_at(Instant.now()).build();
+    public static Instructor toEntity(InstructorDto.InstructorCreateReques req) {
+        return Instructor.builder().email(req.email()).fullName(req.name()).created_at(Instant.now()).build();
 
     }
 
-    public static Instructor toEntity(InstructorUpdateReques req) {
+    public static Instructor toEntity(InstructorDto.InstructorUpdateReques req) {
 
-        return Instructor.builder().id(req.id()).email(req.mail()).fullName(req.name()).updated_at(Instant.now())
+        return Instructor.builder().id(req.id()).email(req.email()).fullName(req.name()).updated_at(Instant.now())
                 .build();
-
     }
 
-    public static instructor toResponse(Instructor entity) {
-        return new InstructorResponse(entity.getId(), entity.getFullName(), entity.getEmail());
+    public static Instructor toResponse(Instructor entity) {
+        return new InstructorDto.InstructorResponse(entity.getId(), entity.getFullName(), entity.getEmail());
     }
 }
